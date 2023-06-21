6c5bbfc863560ad9b45882caa3a5bde856138752
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-06-21 12:28:43 -0700

    adding docs and notes

--- pyegt/height.py
+++ pyegt/height.py
@@ -7,6 +7,51 @@ from . import defs
 from . import utils
 
 class HeightModel:
+    """
+    Look up the geoid, tidal, or geopotential model height above the ellipsoid
+    in order to convert model-referenced heights to ellipsoid height (i.e.
+    compatible with Cesium) and vice-versa.
+
+    The following figure demonstrates the difference between geoid, ellipsoid,
+    and topographic ground surface:
+
+    .. figure:: https://user-images.githubusercontent.com/18689918/239385604-5b5dd0df-e2fb-4ea9-90e7-575287a069e6.png
+        :align: center
+
+        Diagram showing conceptual model of ellipsoid height ``h``, geoid
+        height ``H``, and height of geoid relative to ellipsoid ``N``
+        along with topographic surface (grey).
+
+    Ellipsoidal height (``h``) is generally used in global projections such as
+    Cesium due to its small digital footprint and ease of calculation relative
+    to systems based on gravity or geopotential height. However, gravity and
+    tides are influenced by local differences in Earth's density and other
+    factors. Therefore some projects prefer reference systems that use height
+    referenced to a geoid or tidal model (``H``) which provides a much easier
+    framework to understand height relative to, for example, modeled mean sea
+    level or sea level potential. Converting from ``H`` to ``h`` requires
+    knowing the height difference between the geoid and the ellipsoid (``N``).
+    Conversion is then a simple addition of these values (``H + N = h``).
+
+    The lookup performed by this :class:`pyegt.height.HeightModel` class will
+    use either the `NGS <https://www.ngs.noaa.gov/web_services/geoid.shtml>`_
+    or `VDatum <https://vdatum.noaa.gov/docs/services.html>`_ API to look up
+    model height relative to the ellipsoid (``N``).
+
+    Variables:
+    :param float lat: Decimal latitude
+    :param float lon: Decimal longitude
+    :param region: Region (for list see :py:func:`pyegt.height.HeightModel.from_model`)
+    :type region: str or None
+    :param from_model: Model (for list see :py:func:`pyegt.height.HeightModel.available_regions`)
+    :type from_model: str or None
+    :param from_vrs: The vertical reference system to convert from (``CRS.is_vertical`` must be true for the main CRS or a sub-CRS)
+    :type from_vrs: pyproj.CRS or None
+    :param from_wkt: Use well-known text to get a VRS to convert from
+    :type from_wkt: str or None
+    :param from_epsg: Use an EPSG code to get a VRS to convert from
+    :type from_epsg: str or None
+    """
     def __init__(self,
                  lat: float,
                  lon: float,
@@ -40,6 +85,32 @@ class HeightModel:
     
     def from_model(self, model: Union[str, Literal[None]]=None):
         """
+        Compute the height above the ellipsoid for the given model.
+        Will set ``self.model`` if the ``model`` variable is supplied.
+        Finally, it will call ``self.get_height()``.
+
+        Valid model values are:
+
+            # using NGS API
+            ['GEOID99', 'G99SSS', 'GEOID03', 'USGG2003', 'GEOID06', 'USGG2009',
+            'GEOID09', 'XUSHG', 'USGG2012', 'GEOID12A', 'GEOID12B', 'GEOID18',]
+
+            # using VDatum API
+            ['NAVD88', 'NGVD29', 'ASVD02', 'W0_USGG2012', 'GUVD04', 'NMVD03',
+            'PRVD02', 'VIVD09', 'CRD', 'EGM2008', 'EGM1996', 'EGM1984',
+            'XGEOID16B', 'XGEOID17B', 'XGEOID18B', 'XGEOID19B', 'XGEOID20B',
+            'IGLD85', 'LWD_IGLD85', 'OHWM_IGLD85', 'CRD', 'LMSL', 'MLLW',
+            'MLW', 'MTL', 'DTL', 'MHW', 'MHHW', 'LWD', 'NAD27', 'NAD83_1986',
+            'NAD83_2011', 'NAD83_NSRS2007', 'NAD83_MARP00', 'NAD83_PACP00',
+            'WGS84_G1674', 'ITRF2014', 'IGS14', 'ITRF2008', 'IGS08',
+            'ITRF2005', 'IGS2005', 'WGS84_G1150', 'ITRF2000', 'IGS00', 'IGb00',
+            'ITRF96', 'WGS84_G873', 'ITRF94', 'ITRF93', 'ITRF92', 'SIOMIT92',
+            'WGS84_G730', 'ITRF91', 'ITRF90', 'ITRF89', 'ITRF88',
+            'WGS84_TRANSIT', 'WGS84_G1762', 'WGS84_G2139']
+
+        Documentation can be found here for the
+        `NGS <https://www.ngs.noaa.gov/web_services/geoid.shtml>`_ and
+        `VDatum <https://vdatum.noaa.gov/docs/services.html>`_ APIs.
         """
         if (not model) and (not self.model):
             raise AttributeError('Cannot calculate model height, as no model has been set.')
@@ -49,6 +120,7 @@ class HeightModel:
         
     def from_vrs(self, vrs: Union[CRS, Literal[None]]):
         """
+
         """
         if (not vrs) and (not self.vrs):
             raise AttributeError('Cannot calculate VRS height, as no VRS has been set.')
@@ -60,19 +132,21 @@ class HeightModel:
                     self.vrs = rs
         elif self.vrs.is_vertical:
             self.vrs = vrs
-        self.from_model(self.vrs.name)
+        return self.from_model(self.vrs.name)
+
     def from_wkt(self, wkt: Union[str, Literal[None]]):
         """
         """
         rs = CRS.from_wkt(wkt)
-        self.from_vrs(rs)
+        return self.from_vrs(rs)
+
     def from_epsg(self, epsg: Union[str, int, Literal[None]]):
         """
         """
         rs = CRS.from_epsg(epsg)
-        self.from_vrs(rs)
+        return self.from_vrs(rs)
 
-    def verify_model(self):
+    def verify_model(self) -> bool:
         """
 
         Variables:
@@ -85,7 +159,7 @@ class HeightModel:
                 return True
         return False
 
-    def get_height(self):
+    def get_height(self) -> float:
         """
         """
         if not self.verify_model():
@@ -106,7 +180,7 @@ class HeightModel:
                 vdatum_json = utils.get_vdatum_json(lat=self.lat, lon=self.lon, vdatum_model=self.vrs, region=defs.REGIONS[0])
                 return float(vdatum_json['t_z'])
 
-    def in_feet_us_survey(self):
+    def in_feet_us_survey(self) -> float:
         """
         Convert to US survey foot (for imperial feet use `in_feet()`).
         1 imperial foot = 0.999998 US survey feet
@@ -114,7 +188,7 @@ class HeightModel:
         if self.height:
             return self.height * 3.2808333333
 
-    def in_feet(self):
+    def in_feet(self) -> float:
         """
         Convert to imperial foot (for US survey feet use `in_feet_us_survey()`).
         1 imperial foot = 0.999998 US survey feet
@@ -122,7 +196,7 @@ class HeightModel:
         if self.height:
             return self.height * 3.280839895
     
-    def in_cm(self):
+    def in_cm(self) -> float:
         """
         Convert to centimeters.
         """
@@ -138,15 +212,22 @@ class HeightModel:
         else:
             return 'n/a'
 
-    def available_models():
+    def available_models(self) -> list:
         """
         Return a list of available geoid and tidal models.
         """
         return defs.MODEL_LIST
 
-    def available_regions():
+    def available_regions(self) -> list:
         """
         Return a list of regions usable with VDatum models.
+        See `VDatum regions <https://vdatum.noaa.gov/docs/services.html#step140>`_
+        in the docs.
+
+        Regions:
+
+            ['contiguous', 'ak', 'seak', 'as', 'chesapeak_delaware',
+            'westcoast', 'gcnmi', 'hi', 'prvi', 'sgi', 'spi', 'sli']
         """
         return defs.REGIONS
 
@@ -155,12 +236,7 @@ class HeightModel:
         Convert to printable representation.
         """
         if self.height:
-            if self.region:
-                return "HeightModel(model='%s', lat=%s, lon=%s, region='%s') -> %s meters" % (self.model, self.lat, self.lon, self.region, self.height)
-            else:
-                return "HeightModel(model='%s', lat=%s, lon=%s) -> %s meters" % (self.model, self.lat, self.lon, self.height)
-        if self.lat and self.lon and self.model:
-            return "HeightModel(model='%s', lat=%s, lon=%s)" % (self.model, self.lat, self.lon)
+            return "HeightModel(model='%s', lat=%s, lon=%s, region='%s', ) -> %s meters" % (self.model, self.lat, self.lon, self.region, self.height)
         else:
             return "HeightModel()"
 
