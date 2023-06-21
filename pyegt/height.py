88c8217a57d67e893c9182c3213686a7e3e7c634
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-06-21 10:25:23 -0700

    adding some useful class definitions

--- pyegt/height.py
+++ pyegt/height.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Union, Literal
 from pyproj import CRS
+from math import isclose
 
 from . import defs
 from . import utils
@@ -104,3 +105,66 @@ class HeightModel:
             else:
                 vdatum_json = utils.get_vdatum_json(lat=self.lat, lon=self.lon, vdatum_model=self.vrs, region=defs.REGIONS[0])
                 return float(vdatum_json['t_z'])
+
+    def in_feet_us_survey(self):
+        """
+        Convert to US survey foot (for imperial feet use `in_feet()`).
+        1 imperial foot = 0.999998 US survey feet
+        """
+        if self.height:
+            return self.height * 3.2808333333
+
+    def in_feet(self):
+        """
+        Convert to imperial foot (for US survey feet use `in_feet_us_survey()`).
+        1 imperial foot = 0.999998 US survey feet
+        """
+        if self.height:
+            return self.height * 3.280839895
+    
+    def in_cm(self):
+        """
+        Convert to centimeters.
+        """
+        if self.height:
+            return self.height * 100
+
+    def __str__(self) -> str:
+        """
+        Convert to string.
+        """
+        if self.height:
+            return '%s' % (self.height)
+        else:
+            return 'n/a'
+
+    def __repr__(self) -> str:
+        """
+        """
+        if self.height:
+            if self.region:
+                return "HeightModel(model='%s', lat=%s, lon=%s, region='%s') -> %s meters" % (self.model, self.lat, self.lon, self.region, self.height)
+            else:
+                return "HeightModel(model='%s', lat=%s, lon=%s) -> %s meters" % (self.model, self.lat, self.lon, self.height)
+        if self.lat and self.lon and self.model:
+            return "HeightModel(model='%s', lat=%s, lon=%s)" % (self.model, self.lat, self.lon)
+        else:
+            return "HeightModel()"
+
+    def __float__(self) -> float:
+        """
+        Convert to float.
+        """
+        if self.height:
+            return self.height
+        else:
+            raise ValueError('Cannot convert to float when no height has been calculated.')
+        
+    def __eq__(self, __value: object) -> bool:
+        """
+        Use `math.isclose()` to compare as floating point numbers are not always directly comparable.
+        """
+        if self.height:
+            return isclose(self.height, float(__value), rel_tol=1e-9, abs_tol=0.0)
+        else:
+            raise ValueError('Cannot compare when no height has been calculated.')
